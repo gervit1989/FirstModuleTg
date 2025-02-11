8a3c16d1e1b9444ae3420db86db63e46fb78ce85
gervit1989 <gervit2008@mail.ru>
2025-02-11 20:04:49 +0300

    Настраиваем тг бота

---
+++ Handlers/CommandHandlers.py
@@ -0,0 +1,23 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from Keyboard import keyboard_start
+
+command_router = Router()
+
+@command_router.message(Command('start'))
+async def com_start(message: Message):
+    await message.answer(
+        text=f'Привет,{message.from_user.full_name}',
+        reply_markup=keyboard_start(),
+    )
+    # for item in dict(message).items():
+    #     print(item)
+
+
+@command_router.message(Command('help'))
+async def com_help(message: Message):
+    await message.answer(
+        text=f'Привет,{message.from_user.full_name}'
+    )
